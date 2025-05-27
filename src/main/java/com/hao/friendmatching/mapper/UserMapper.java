1491adbd210bddb3daaecfd377edf334399676e2
haodada <3382462827@qq.com>
2025-05-27 21:39:33 +0800

    初始化

---
+++ src/main/java/com/hao/friendmatching/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.hao.friendmatching.mapper;
+
+import com.hao.friendmatching.model.domain.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 用户 Mapper
+ *
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
